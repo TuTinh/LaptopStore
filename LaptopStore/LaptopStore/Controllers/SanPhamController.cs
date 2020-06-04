5671400c4c171d49299b896ecb7f137abe0d930c
ngoctan7d@gmail.com <ngoctan7d@gmail.com>
2020-06-04 21:40:13 +0700

    Chỉnh sửa phần sanpham Controller

--- LaptopStore/LaptopStore/Controllers/SanPhamController.cs
+++ LaptopStore/LaptopStore/Controllers/SanPhamController.cs
@@ -100,11 +100,11 @@ namespace LaptopStore.Controllers
         [ChildActionOnly]
         public PartialViewResult HeaderCart()
         {
-            var cart = Session[CommonConstant.CartSession];
+            var cart = (Cart)Session[CommonConstant.CartSession];
             var list = new List<CartItem>();
             if (cart != null)
             {
-                list = (List<CartItem>)cart;
+                list = cart.Lines.ToList();
             }
             return PartialView(list);
         }
